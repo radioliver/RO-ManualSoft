537017f62be973a09c9eda20f52266b47ae33940
radioliver <radi.oliver@diak.szbi-pg.hu>
2026-01-18 20:16:10 +0100

    Work

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -2,119 +2,105 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        private System.Windows.Forms.ListBox listBoxEszkozok;
+        private System.Windows.Forms.Button btnManual;
+        private System.Windows.Forms.Button btnSzamla;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelNyelv;
+        private AxAcroPDFLib.AxAcroPDF axAcroPDF1;
+
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
-            {
                 components.Dispose();
-            }
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.termekek = new System.Windows.Forms.ToolStripMenuItem();
-            this.hozzáasásToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.törlésToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.osmoPocket3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.osmoPocket3ToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
-            this.menuStrip1.SuspendLayout();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBoxEszkozok = new System.Windows.Forms.ListBox();
+            this.btnManual = new System.Windows.Forms.Button();
+            this.btnSzamla = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelNyelv = new System.Windows.Forms.Label();
+            this.axAcroPDF1 = new AxAcroPDFLib.AxAcroPDF();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axAcroPDF1)).BeginInit();
             this.SuspendLayout();
             // 
-            // menuStrip1
+            // listBoxEszkozok
             // 
-            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 15F);
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.termekek,
-            this.hozzáasásToolStripMenuItem,
-            this.törlésToolStripMenuItem});
-            this.menuStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1904, 36);
-            this.menuStrip1.TabIndex = 0;
-            this.menuStrip1.Text = "menuStrip1";
+            this.listBoxEszkozok.ItemHeight = 16;
+            this.listBoxEszkozok.Location = new System.Drawing.Point(20, 20);
+            this.listBoxEszkozok.Name = "listBoxEszkozok";
+            this.listBoxEszkozok.Size = new System.Drawing.Size(250, 292);
+            this.listBoxEszkozok.TabIndex = 0;
+            this.listBoxEszkozok.SelectedIndexChanged += new System.EventHandler(this.listBoxEszkozok_SelectedIndexChanged);
             // 
-            // termekek
+            // btnManual
             // 
-            this.termekek.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.osmoPocket3ToolStripMenuItem});
-            this.termekek.Name = "termekek";
-            this.termekek.Size = new System.Drawing.Size(70, 20);
-            this.termekek.Text = "Termékek";
-            this.termekek.Click += new System.EventHandler(this.egyToolStripMenuItem_Click);
+            this.btnManual.Location = new System.Drawing.Point(300, 20);
+            this.btnManual.Name = "btnManual";
+            this.btnManual.Size = new System.Drawing.Size(120, 30);
+            this.btnManual.TabIndex = 3;
+            this.btnManual.Text = "Kézikönyv";
+            this.btnManual.Click += new System.EventHandler(this.btnManual_Click);
             // 
-            // hozzáasásToolStripMenuItem
+            // btnSzamla
             // 
-            this.hozzáasásToolStripMenuItem.Name = "hozzáasásToolStripMenuItem";
-            this.hozzáasásToolStripMenuItem.Size = new System.Drawing.Size(73, 20);
-            this.hozzáasásToolStripMenuItem.Text = "Hozzáasás";
+            this.btnSzamla.Location = new System.Drawing.Point(300, 60);
+            this.btnSzamla.Name = "btnSzamla";
+            this.btnSzamla.Size = new System.Drawing.Size(120, 30);
+            this.btnSzamla.TabIndex = 4;
+            this.btnSzamla.Text = "Számla";
+            this.btnSzamla.Click += new System.EventHandler(this.btnSzamla_Click);
             // 
-            // törlésToolStripMenuItem
+            // pictureBox1
             // 
-            this.törlésToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.osmoPocket3ToolStripMenuItem2});
-            this.törlésToolStripMenuItem.Name = "törlésToolStripMenuItem";
-            this.törlésToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
-            this.törlésToolStripMenuItem.Text = "Törlés";
+            this.pictureBox1.Location = new System.Drawing.Point(300, 110);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
             // 
-            // osmoPocket3ToolStripMenuItem
+            // labelNyelv
             // 
-            this.osmoPocket3ToolStripMenuItem.Name = "osmoPocket3ToolStripMenuItem";
-            this.osmoPocket3ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.osmoPocket3ToolStripMenuItem.Text = "Osmo Pocket 3";
-            this.osmoPocket3ToolStripMenuItem.Click += new System.EventHandler(this.osmoPocket3ToolStripMenuItem_Click);
+            this.labelNyelv.AutoSize = true;
+            this.labelNyelv.Location = new System.Drawing.Point(300, 320);
+            this.labelNyelv.Name = "labelNyelv";
+            this.labelNyelv.Size = new System.Drawing.Size(0, 16);
+            this.labelNyelv.TabIndex = 6;
             // 
-            // osmoPocket3ToolStripMenuItem2
+            // axAcroPDF1
             // 
-            this.osmoPocket3ToolStripMenuItem2.Name = "osmoPocket3ToolStripMenuItem2";
-            this.osmoPocket3ToolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
-            this.osmoPocket3ToolStripMenuItem2.Text = "Osmo Pocket 3";
+            this.axAcroPDF1.Enabled = true;
+            this.axAcroPDF1.Location = new System.Drawing.Point(630, 20);
+            this.axAcroPDF1.Name = "axAcroPDF1";
+            this.axAcroPDF1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axAcroPDF1.OcxState")));
+            this.axAcroPDF1.Size = new System.Drawing.Size(848, 696);
+            this.axAcroPDF1.TabIndex = 7;
+            this.axAcroPDF1.Visible = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1904, 1041);
-            this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
-            this.MaximumSize = new System.Drawing.Size(1920, 1080);
-            this.MinimumSize = new System.Drawing.Size(1918, 1030);
+            this.ClientSize = new System.Drawing.Size(1564, 781);
+            this.Controls.Add(this.listBoxEszkozok);
+            this.Controls.Add(this.btnManual);
+            this.Controls.Add(this.btnSzamla);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelNyelv);
+            this.Controls.Add(this.axAcroPDF1);
             this.Name = "Form1";
-            this.Text = "RO-ManualSoft";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
+            this.Text = "RO ManualSoft";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axAcroPDF1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
-
-        #endregion
-
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem termekek;
-        private System.Windows.Forms.ToolStripMenuItem hozzáasásToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem törlésToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem osmoPocket3ToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem osmoPocket3ToolStripMenuItem2;
     }
 }
-
--- Form1.cs
+++ Form1.cs
@@ -1,70 +1,236 @@
 ﻿using AxAcroPDFLib;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace RO_ManualSoft
 {
     public partial class Form1 : Form
     {
-        PictureBox pb = new PictureBox();
-        Button pdf = new Button();
-        Button szamla = new Button();
-        AxAcroPDFLib.AxAcroPDF reader = new AxAcroPDFLib.AxAcroPDF();
+        List<Eszkoz> eszkozok = new List<Eszkoz>();
+        Eszkoz aktualis;
+
+        // -------- Hozzáadás panel vezérlők --------
+        TextBox tbNev;
+        TextBox tbKep;
+        TextBox tbManual;
+        TextBox tbSzamla;
+        ComboBox cbNyelv;
+        Button btnBrowseKep;
+        Button btnBrowseManual;
+        Button btnBrowseSzamla;
+        Button btnAddOk;
+        Button btnAddCancel;
+        Panel panelHozzaad;
 
         public Form1()
         {
             InitializeComponent();
-            pdf.Visible = false;
-            szamla.Visible = false;
-            pdf.Visible = false;
-            pdf.Click += Pdf_Click;
-
+            AlapAdatBetoltes();
+            InitHozzaadPanel();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        void AlapAdatBetoltes()
         {
-
+            Eszkoz e = new Eszkoz()
+            {
+                Nev = "Osmo Pocket 3",
+                Tipus = "Kamera",
+                ManualPdf = "OsmoPocket3_manual.pdf",
+                ManualNyelv = "Magyar",
+                SzamlaPdf = "OsmoPocket3_szamla.pdf",
+                Kep = "OsmoPocket3.jpg"
+            };
+            Eszkoz f = new Eszkoz()
+            {
+                Nev = "Logitech G502 Hero",
+                Tipus = "Egér",
+                ManualPdf = "G502Hero_manual.pdf",
+                ManualNyelv = "Angol",
+                SzamlaPdf = "G502Hero_szamla.pdf",
+                Kep = "G502Hero.png"
+            };
+            Eszkoz g = new Eszkoz()
+            {
+                Nev = "Dyson V15",
+                Tipus = "Porszívó",
+                ManualPdf = "Dysonv15_manual.pdf",
+                ManualNyelv = "Magyar",
+                SzamlaPdf = "Dysonv15_szamla.pdf",
+                Kep = "Dysonv15.jpeg"
+            };
+            Eszkoz h = new Eszkoz()
+            {
+                Nev = "Mas***bator Pro",
+                Tipus = "",
+                ManualPdf = "MasturbatorPro_manual.pdf",
+                ManualNyelv = "Magyar",
+                SzamlaPdf = "MasturbatorPro_szamla.pdf",
+                Kep = "MasturbatorPro.jpg"
+            };
+            eszkozok.Add(h);
+            listBoxEszkozok.Items.Add(h);
+            eszkozok.Add(g);
+            listBoxEszkozok.Items.Add(g);
+            eszkozok.Add(f);
+            listBoxEszkozok.Items.Add(f);
+            eszkozok.Add(e);
+            listBoxEszkozok.Items.Add(e);
         }
 
-        private void egyToolStripMenuItem_Click(object sender, EventArgs e)
+        private void listBoxEszkozok_SelectedIndexChanged(object sender, EventArgs e)
         {
-        
+            if (listBoxEszkozok.SelectedIndex == -1) return;
+
+            aktualis = (Eszkoz)listBoxEszkozok.SelectedItem;
+            pictureBox1.Image = File.Exists(aktualis.Kep) ? Image.FromFile(aktualis.Kep) : null;
+            labelNyelv.Text = "Kézikönyv nyelve: " + aktualis.ManualNyelv;
+            axAcroPDF1.Visible = false;
         }
 
-        private void osmoPocket3ToolStripMenuItem_Click(object sender, EventArgs e)
+        private void btnManual_Click(object sender, EventArgs e)
         {
-            pdf.Text = "Kézikönyv";
-            pdf.Location = new Point(50, 400);
-
-            this.Controls.Add(pdf);
-            pdf.Visible = true;
-            szamla.Text = "Számla";
-            szamla.Location = new Point(170, 580);
-            szamla.Visible = true;
-            this.Controls.Add(szamla);
-            pb.Image = Image.FromFile($"{osmoPocket3ToolStripMenuItem.Text}.jpeg");
-            pb.Location = new Point(50, 150);
-            pb.Visible = true;
-            pb.SizeMode = PictureBoxSizeMode.StretchImage;
-            pb.Size = new Size(400, 400);
-            this.Controls.Add(pb);
-
-
+            if (aktualis != null) PdfMegnyit(aktualis.ManualPdf);
         }
-        private void Pdf_Click(object sender, EventArgs e)
+
+        private void btnSzamla_Click(object sender, EventArgs e)
         {
-            reader.Visible = true;
-            reader.Location = new Point(500, 50);
-            reader.Size = new Size(1400, 900);
-            this.Controls.Add(reader);
-            reader.LoadFile($"Osmo Pocket 3.pdf");
+            if (aktualis != null) PdfMegnyit(aktualis.SzamlaPdf);
         }
+
+        void PdfMegnyit(string fajl)
+        {
+            if (File.Exists(fajl))
+            {
+                axAcroPDF1.LoadFile(fajl);
+                axAcroPDF1.Visible = true;
+            }
+            else
+            {
+                MessageBox.Show("A PDF nem található!");
+            }
+        }
+
+        private void btnTorol_Click(object sender, EventArgs e)
+        {
+            if (listBoxEszkozok.SelectedIndex == -1) return;
+            Eszkoz torlendo = (Eszkoz)listBoxEszkozok.SelectedItem;
+            eszkozok.Remove(torlendo);
+            listBoxEszkozok.Items.Remove(torlendo);
+
+            pictureBox1.Image = null;
+            labelNyelv.Text = "";
+            axAcroPDF1.Visible = false;
+            aktualis = null;
+        }
+
+        private void InitHozzaadPanel()
+        {
+            panelHozzaad = new Panel();
+            panelHozzaad.Size = new Size(400, 250);
+            panelHozzaad.Location = new Point(20, 380);
+            panelHozzaad.BorderStyle = BorderStyle.FixedSingle;
+
+            Label lblNev = new Label() { Text = "Név:", Location = new Point(10, 10), AutoSize = true };
+            tbNev = new TextBox() { Location = new Point(100, 10), Width = 250 };
+
+            Label lblKep = new Label() { Text = "Kép:", Location = new Point(10, 40), AutoSize = true };
+            tbKep = new TextBox() { Location = new Point(100, 40), Width = 200 };
+            btnBrowseKep = new Button() { Text = "Tallóz", Location = new Point(310, 38), Size = new Size(70, 25) };
+            btnBrowseKep.Click += BtnBrowseKep_Click;
+
+            Label lblManual = new Label() { Text = "Manual PDF:", Location = new Point(10, 70), AutoSize = true };
+            tbManual = new TextBox() { Location = new Point(100, 70), Width = 200 };
+            btnBrowseManual = new Button() { Text = "Tallóz", Location = new Point(310, 68), Size = new Size(70, 25) };
+            btnBrowseManual.Click += BtnBrowseManual_Click;
+
+            Label lblSzamla = new Label() { Text = "Számla PDF:", Location = new Point(10, 100), AutoSize = true };
+            tbSzamla = new TextBox() { Location = new Point(100, 100), Width = 200 };
+            btnBrowseSzamla = new Button() { Text = "Tallóz", Location = new Point(310, 98), Size = new Size(70, 25) };
+            btnBrowseSzamla.Click += BtnBrowseSzamla_Click;
+
+            Label lblNyelv = new Label() { Text = "Nyelv:", Location = new Point(10, 130), AutoSize = true };
+            cbNyelv = new ComboBox() { Location = new Point(100, 130), Width = 150 };
+            cbNyelv.Items.AddRange(new string[] { "Magyar", "Angol" });
+            cbNyelv.SelectedIndex = 0;
+
+            btnAddOk = new Button() { Text = "Hozzáadás", Location = new Point(100, 170), Size = new Size(100, 30) };
+            btnAddOk.Click += BtnAddOk_Click;
+
+            btnAddCancel = new Button() { Text = "Mégse", Location = new Point(220, 170), Size = new Size(100, 30) };
+            btnAddCancel.Click += BtnAddCancel_Click;
+
+            panelHozzaad.Controls.AddRange(new Control[] {
+                lblNev, tbNev,
+                lblKep, tbKep, btnBrowseKep,
+                lblManual, tbManual, btnBrowseManual,
+                lblSzamla, tbSzamla, btnBrowseSzamla,
+                lblNyelv, cbNyelv,
+                btnAddOk, btnAddCancel
+            });
+
+            this.Controls.Add(panelHozzaad);
+        }
+
+        private void BtnBrowseKep_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog() { Filter = "Képek|*.jpg;*.png;*.bmp" };
+            if (ofd.ShowDialog() == DialogResult.OK) tbKep.Text = ofd.FileName;
+        }
+
+        private void BtnBrowseManual_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog() { Filter = "PDF|*.pdf" };
+            if (ofd.ShowDialog() == DialogResult.OK) tbManual.Text = ofd.FileName;
+        }
+
+        private void BtnBrowseSzamla_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog() { Filter = "PDF|*.pdf" };
+            if (ofd.ShowDialog() == DialogResult.OK) tbSzamla.Text = ofd.FileName;
+        }
+
+        private void BtnAddOk_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tbNev.Text))
+            {
+                MessageBox.Show("Adj nevet az eszköznek!");
+                return;
+            }
+
+            Eszkoz uj = new Eszkoz()
+            {
+                Nev = tbNev.Text,
+                Kep = tbKep.Text,
+                ManualPdf = tbManual.Text,
+                SzamlaPdf = tbSzamla.Text,
+                ManualNyelv = cbNyelv.SelectedItem.ToString()
+            };
+
+            eszkozok.Add(uj);
+            listBoxEszkozok.Items.Add(uj);
+
+            tbNev.Text = tbKep.Text = tbManual.Text = tbSzamla.Text = "";
+            cbNyelv.SelectedIndex = 0;
+        }
+
+        private void BtnAddCancel_Click(object sender, EventArgs e)
+        {
+            tbNev.Text = tbKep.Text = tbManual.Text = tbSzamla.Text = "";
+            cbNyelv.SelectedIndex = 0;
+        }
+    }
+
+    public class Eszkoz
+    {
+        public string Nev;
+        public string Tipus;
+        public string ManualPdf;
+        public string ManualNyelv;
+        public string SzamlaPdf;
+        public string Kep;
+
     }
 }
